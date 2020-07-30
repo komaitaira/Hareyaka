8b8c956b089efc722185a69483798b106207c357
vagrant <vagrant@localhost.localdomain>
2020-07-30 15:47:11 +0000

    フォロー機能非同期化

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -1,15 +1,13 @@
 class Public::RelationshipsController < ApplicationController
   def create
     follow = current_user.active_relationships.build(follower_id: params[:user_id])
-    @company = Company.where(id: follow.follower_id)
+    @company = Company.find(follow.follower_id)
     follow.save
-    redirect_back(fallback_location: root_path)
   end
 
   def destroy
     follow = current_user.active_relationships.find_by(follower_id: params[:user_id])
-    @company = Company.where(id: follow.follower_id)
+    @company = Company.find(follow.follower_id)
     follow.destroy
-    redirect_back(fallback_location: root_path)
   end
 end
