6fdc766c809e75b811c84a64d7f8cf40d2d61944
vagrant <vagrant@localhost.localdomain>
2020-09-30 23:01:18 +0000

    Fix テストコード修正、labelタグ修正

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -9,31 +9,31 @@ RSpec.describe 'User', type: :system do
       end
       context '新規登録画面に遷移' do
         it '新規登録に成功する' do
-          fill_in 'user[last_name]', with: "テスト"
-          fill_in 'user[first_name]', with: "二郎"
-          fill_in 'user[kana_last_name]', with: "テスト"
-          fill_in 'user[kana_first_name]', with: "ジロウ"
-          fill_in 'user[email]', with: "testjiro@example.com"
-          fill_in 'user[postal_code]', with: "2222222"
-          fill_in 'user[address]', with: "東京都足立区123-12-1"
-          fill_in 'user[phone_number]', with: "22222222222"
-          fill_in 'user[password]', with: "testjiro"
-          fill_in 'user[password_confirmation]', with: "testjiro"
+          fill_in '名前(姓)', with: "テスト"
+          fill_in '名前(名)', with: "二郎"
+          fill_in 'フリガナ(セイ)', with: "テスト"
+          fill_in 'フリガナ(メイ)', with: "ジロウ"
+          fill_in 'メールアドレス', with: "testjiro@example.com"
+          fill_in '郵便番号(ハイフンなし)', with: "2222222"
+          fill_in '住所', with: "東京都足立区123-12-1"
+          fill_in '電話番号(ハイフンなし)', with: "22222222222"
+          fill_in 'パスワード', with: "testjiro"
+          fill_in '確認用パスワード', with: "testjiro"
           click_button '新規登録'
           expect(page).to have_content 'アカウント登録が完了しました。'
         end
         
         it '新規登録に失敗する' do
-          fill_in 'user[last_name]', with: ""
-          fill_in 'user[first_name]', with: ""
-          fill_in 'user[kana_last_name]', with: ""
-          fill_in 'user[kana_first_name]', with: ""
-          fill_in 'user[email]', with: ""
-          fill_in 'user[postal_code]', with: ""
-          fill_in 'user[address]', with: ""
-          fill_in 'user[phone_number]', with: ""
-          fill_in 'user[password]', with: ""
-          fill_in 'user[password_confirmation]', with: ""
+          fill_in '名前(姓)', with: ""
+          fill_in '名前(名)', with: ""
+          fill_in 'フリガナ(セイ)', with: ""
+          fill_in 'フリガナ(メイ)', with: ""
+          fill_in 'メールアドレス', with: ""
+          fill_in '郵便番号(ハイフンなし)', with: ""
+          fill_in '住所', with: ""
+          fill_in '電話番号(ハイフンなし)', with: ""
+          fill_in 'パスワード', with: ""
+          fill_in '確認用パスワード', with: ""
           click_button '新規登録'
           expect(page).to have_content '個人会員 は保存されませんでした。'
         end
@@ -54,8 +54,8 @@ RSpec.describe 'User', type: :system do
         end
 
         it 'ログインに失敗する' do
-          fill_in 'user[email]', with: ''
-          fill_in 'user[password]', with: ''
+          fill_in 'メールアドレス', with: ''
+          fill_in 'パスワード', with: ''
           click_button 'ログイン'
           expect(current_path).to eq(new_user_session_path)
         end
@@ -66,8 +66,8 @@ RSpec.describe 'User', type: :system do
   describe 'ユーザーのテスト' do
     before do
       visit new_user_session_path
-      fill_in 'user[email]', with: user.email
-      fill_in 'user[password]', with: user.password
+      fill_in 'メールアドレス', with: user.email
+      fill_in 'パスワード', with: user.password
       click_button 'ログイン'
     end
 
@@ -100,36 +100,36 @@ RSpec.describe 'User', type: :system do
           expect(page).to have_field 'user[profile_image]'
         end
         it '名前編集フォームに自分の姓が表示される' do
-          expect(page).to have_field 'user[last_name]', with: user.last_name
+          expect(page).to have_field '名前(姓)', with: user.last_name
         end
         it '名前編集フォームに自分の名が表示される' do
-          expect(page).to have_field 'user[first_name]', with: user.first_name
+          expect(page).to have_field '名前(名)', with: user.first_name
         end
         it '名前編集フォームに自分のカナ姓が表示される' do
-          expect(page).to have_field 'user[kana_last_name]', with: user.kana_last_name
+          expect(page).to have_field 'フリガナ(セイ)', with: user.kana_last_name
         end
         it '名前編集フォームに自分のカナ名が表示される' do
-          expect(page).to have_field 'user[kana_first_name]', with: user.kana_first_name
+          expect(page).to have_field 'フリガナ(メイ)', with: user.kana_first_name
         end
         it 'メールアドレス編集フォームに自分のメールアドレスが表示される' do
-          expect(page).to have_field 'user[email]', with: user.email
+          expect(page).to have_field 'メールアドレス', with: user.email
         end
         it '郵便番号編集フォームに自分の郵便番号が表示される' do
-          expect(page).to have_field 'user[postal_code]', with: user.postal_code
+          expect(page).to have_field '郵便番号(ハイフンなし)', with: user.postal_code
         end
         it '住所編集フォームに自分の住所が表示される' do
-          expect(page).to have_field 'user[address]', with: user.address
+          expect(page).to have_field '住所', with: user.address
         end
         it '電話番号編集フォームに自分の電話番号が表示される' do
-          expect(page).to have_field 'user[phone_number]', with: user.phone_number
+          expect(page).to have_field '電話番号(ハイフンなし)', with: user.phone_number
         end
         it '自己紹介文編集フォームに自分の自己紹介文が表示される' do
-          expect(page).to have_field 'user[introduction]', with: user.introduction
+          expect(page).to have_field '自己紹介文', with: user.introduction
         end
         it '編集に成功する' do
           # 名前を三郎に変更
           fill_in 'user[first_name]', with: '三郎'
-          fill_in 'user[kana_first_name]', with: 'サブロウ'
+          fill_in 'フリガナ(メイ)', with: 'サブロウ'
           click_button '変更を保存する'
           expect(page).to have_content '会員情報の更新が完了しました。'
           expect(page).to have_content 'テスト 三郎 (テスト サブロウ)'
